df2815a643e38c2433b5c10e563d0d88649a01a0
sachin <sachinbhattarai55@gmail.com>
2026-07-25 11:27:07 +0545

    feat: added different bg for different themes

---
+++ src/constants/backgroundImages.ts
@@ -0,0 +1,19 @@
+import darkJPG from "@/assets/bg_dark.jpg";
+import lightJPG from "@/assets/bg_light.jpg";
+import bgOrangeDarkJPG from "@/assets/bg_orange_dark.jpg";
+import bgOrangeLightJPG from "@/assets/bg_orange_light.jpg";
+import bgPurpleDarkJPG from "@/assets/bg_purple_dark.jpg";
+import bgPurpleLightJPG from "@/assets/bg_purple_light.jpg";
+
+export const bgImages = {
+  dark: {
+    default: darkJPG,
+    orange: bgOrangeDarkJPG,
+    purple: bgPurpleDarkJPG,
+  },
+  light: {
+    default: lightJPG,
+    orange: bgOrangeLightJPG,
+    purple: bgPurpleLightJPG,
+  },
+};
